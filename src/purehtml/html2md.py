be3c169dc02d0d029f08cd5e1b75a250b5e3dc4c
Hansimov <591172499@qq.com>
2024-03-13 17:54:05 +0800

    :recycle: [Refactor] Rewrite find_all by tag types, and upgrade to v0.6.3

--- src/purehtml/html2md.py
+++ src/purehtml/html2md.py
@@ -21,7 +21,6 @@ BEGIN_MARK_MAP = {
     "h5": "#####",
     "h6": "######",
     "li": "-",
-    "ol": "1.",
 }
 PAIRED_MARK_MAP = {
     "b": "**",
@@ -52,52 +51,47 @@ def html2md(html_str):
 
     soup = BeautifulSoup(html_str, "html.parser")
 
-    for element in soup.contents:
-        if element.text.strip() == "":
-            element.extract()
-
-    for element in soup.find_all():
-        try:
-            if element.name in UNWRAP_TAGS:
-                element.unwrap()
-            elif element.name in GROUP_TAGS + LIST_BEGIN_TAGS:
-                element.insert_before("\n")
-                element.insert_after("\n")
-                element.unwrap()
-            elif element.name in BEGIN_MARK_MAP:
-                mark = BEGIN_MARK_MAP[element.name]
-                new_string = str(element).strip()
-                replaced_map = {
-                    rf"^<{element.name}.*?>": "",
-                    rf"</{element.name}>$": "",
-                    "\n+": " ",
-                }
-                for pattern, replaced in replaced_map.items():
-                    new_string = re.sub(pattern, replaced, new_string)
-                new_string = new_string.strip()
-                new_string = f"{mark} {new_string}"
-                new_string = re.sub(rf"{mark}\s*•", f"{mark}", new_string)
-                element.replace_with(BeautifulSoup(new_string, "html.parser"))
-            elif element.name in PAIRED_MARK_MAP:
-                mark = PAIRED_MARK_MAP[element.name]
-                element.insert_before(mark)
-                element.insert_after(mark)
-                element.unwrap()
-            elif element.name in PER_LINE_MARK_MAP:
-                mark = PER_LINE_MARK_MAP[element.name]
-                lines = str(element).split("\n")
-                marked_lines = [f"{mark} {line}" for line in lines]
-                new_string = "\n".join(marked_lines)
-                element.replace_with(BeautifulSoup(new_string, "html.parser"))
-            elif element.name in NEW_LINE_TAGS:
-                element.insert_before("\n")
-                element.insert_after("\n")
-            else:
-                pass
-        except Exception as e:
-            print(f"x {e}")
-            print(element)
-            continue
+    try:
+        for element in soup.contents:
+            if element.text.strip() == "":
+                element.extract()
+        for element in soup.find_all(UNWRAP_TAGS):
+            element.unwrap()
+        for element in soup.find_all(GROUP_TAGS + LIST_BEGIN_TAGS):
+            element.insert_before("\n")
+            element.insert_after("\n")
+            element.unwrap()
+        for element in soup.find_all(BEGIN_MARK_MAP.keys()):
+            mark = BEGIN_MARK_MAP[element.name]
+            new_string = str(element).strip()
+            replaced_map = {
+                rf"^<{element.name}.*?>": "",
+                rf"</{element.name}>$": "",
+                "\n+": " ",
+            }
+            for pattern, replaced in replaced_map.items():
+                new_string = re.sub(pattern, replaced, new_string)
+            new_string = new_string.strip()
+            new_string = f"{mark} {new_string}"
+            new_string = re.sub(rf"{mark}\s*•", f"{mark}", new_string)
+            element.replace_with(BeautifulSoup(new_string, "html.parser"))
+        for element in soup.find_all(PAIRED_MARK_MAP.keys()):
+            mark = PAIRED_MARK_MAP[element.name]
+            element.insert_before(mark)
+            element.insert_after(mark)
+            element.unwrap()
+        for element in soup.find_all(PER_LINE_MARK_MAP.keys()):
+            mark = PER_LINE_MARK_MAP[element.name]
+            lines = str(element).split("\n")
+            marked_lines = [f"{mark} {line}" for line in lines]
+            new_string = "\n".join(marked_lines)
+            element.replace_with(BeautifulSoup(new_string, "html.parser"))
+        for element in soup.find_all(NEW_LINE_TAGS):
+            element.insert_before("\n")
+            element.insert_after("\n")
+    except Exception as e:
+        print(f"x {e}")
+        print(element)
 
     md_str = str(soup)
     md_str = re.sub(r"\n{3,}", "\n\n", md_str)
